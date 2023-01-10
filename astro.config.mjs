fff357a17d287b3f1afb1df8714334c66b22605c
Anurag Bhandari <anurag.bhd@gmail.com>
2023-01-10 07:09:57 +0530

    Allow embedding YouTube videos in posts

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,6 @@
 import { defineConfig } from 'astro/config';
 import lit from '@astrojs/lit';
+import mdx from '@astrojs/mdx';
 import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
@@ -8,6 +9,6 @@ export default defineConfig({
   // Your public domain, e.g.: https://my-site.dev/. Used to generate sitemaps and canonical URLs.
   sitemap: true,
   // Generate sitemap (set to "false" to disable)
-  integrations: [lit(), sitemap()] // Add renderers to the config
+  integrations: [lit(), mdx(), sitemap()] // Add renderers to the config
 
 });
